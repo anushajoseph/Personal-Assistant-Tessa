5cd62b07025c4c04bf431a075ebd43fab6cf7d25
Sandra <sandraantony2002@gmail.com>
2021-09-23 11:12:46 +0530

    added talking and listening functions

--- Tessa.py
+++ Tessa.py
@@ -4,3 +4,26 @@ import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[1].id)
+
+def talk(text):
+engine.say(text)
+            engine.runAndWait()
+
+def take_command():
+try:
+    with sr.Microphone() as source:
+        print('Tessa listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'tessa' in command:
+            print(command)
+
+            except:
+            pass
+        return command
