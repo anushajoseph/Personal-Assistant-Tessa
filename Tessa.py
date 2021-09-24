5a41f55e760f0cf0a46e0541217892a2c5485413
sneha2180 <snehacshaji202@gmail.com>
2021-09-25 03:11:09 +0530

    Command error rectified

--- Tessa.py
+++ Tessa.py
@@ -13,34 +13,33 @@ engine.say('Hey I am Tessa ,How can I help you')
 engine.runAndWait()
 
 def talk(text):
-            engine.say(text)
-            engine.runAndWait()
+    engine.say(text)
+    engine.runAndWait()
 
 def take_command():
     try:
         with sr.Microphone() as source:
-            print('Tessa listening...')
+            print('listening ......')
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-        if 'tessa' in command:
-            command = command.replace('tessa', '')
-            print(command)
-
+            if 'alexa'in command:
+                command = command.replace('alexa', '')
+                print(command)
+        return command
     except:
-            pass
-    return command
+        pass
+
 
 def run_tessa():
     command = take_command()
     print(command)
     if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
+        song = command.replace('play','')
+        talk('playing'+song)
         pywhatkit.playonyt(song)
     else:
-        talk('Please say the command again.')
-
+        talk('Please say the command again')
 
 while True:
     run_tessa()
